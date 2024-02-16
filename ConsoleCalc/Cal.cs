9cb095fcdb3a419c55d6ea5271dc4361d3b43434
StandaKotov <dp201090ksj@gmail.com>
2024-02-15 20:25:30 -0600

    ConsoleCalc

---
+++ ConsoleCalc/Cal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    public class Calculator1
+    {
+        // Метод для сложения двух чисел
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Метод для вычитания двух чисел
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Метод для умножения двух чисел
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        // Метод для деления двух чисел
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Ошибка: деление на ноль!");
+                return 0;
+            }
+            return (double)a / b;
+        }
+    }
+}
--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -1,31 +1,76 @@
 ﻿using System;
 using System.Text;
-
+namespace ConsoleCalc;
 class Calculator
 {
     static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
-        while (true)
+        Calculator1 calc = new Calculator1();
+
+        Console.WriteLine("Введите первое число:");
+        int num1;
+        if (!int.TryParse(Console.ReadLine(), out num1))
         {
-            Console.WriteLine("Калькулятор:");
-            Console.WriteLine("1. Сложение");
-            Console.WriteLine("2. Вычитание");
-            Console.WriteLine("3. Умножение");
-            Console.WriteLine("4. Деление");
-            Console.WriteLine("5. Выход");
-
-            Console.Write("Выберите операцию (1/2/3/4/5): ");
-            
-            // Код пишем тут
-            // Не забываем про break;
-            // Не забываем про деление на ноль
-            // Не забываем про некорректный ввод (например, буквы)
-
-            //Console.Write("Введите первое число: ");
-
-            //Console.Write("Введите второе число: ");
-
+            Console.WriteLine("Ошибка: введите целое число.");
+            return;
         }
+
+        Console.WriteLine("Введите второе число:");
+        int num2;
+        if (!int.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Ошибка: введите целое число.");
+            return;
+        }
+
+        Console.WriteLine("Выберите операцию:");
+        Console.WriteLine("1. Сложение");
+        Console.WriteLine("2. Вычитание");
+        Console.WriteLine("3. Умножение");
+        Console.WriteLine("4. Деление");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        int result = 0;
+
+        switch (choice)
+        {
+            case 1:
+                result = calc.Add(num1, num2);
+                break;
+            case 2:
+                result = calc.Subtract(num1, num2);
+                break;
+            case 3:
+                result = calc.Multiply(num1, num2);
+                break;
+            case 4:
+                double divisionResult = calc.Divide(num1, num2);
+                if (divisionResult != 0)
+                {
+                    Console.WriteLine("Результат деления: " + divisionResult);
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            default:
+                Console.WriteLine("Неверный выбор операции!");
+                return;
+        }
+
+        Console.WriteLine("Результат операции: " + result);
+
+
+        // Код пишем тут
+        // Не забываем про break;
+        // Не забываем про деление на ноль
+        // Не забываем про некорректный ввод (например, буквы)
+
+        //Console.Write("Введите первое число: ");
+
+        //Console.Write("Введите второе число: ");
+
     }
 }
